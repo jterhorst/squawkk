ada07cb86b2d3cb62dfd574df3655f82dbbcd3f8
Jason Terhorst <yourstruly@jterhorst.com>
2018-12-03 10:24:25 -0600

    First build

--- Squawkk/ViewController.m
+++ Squawkk/ViewController.m
@@ -8,8 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import <AVKit/AVKit.h>
+#import <AVFoundation/AVFoundation.h>
 
+@interface ViewController ()
+{
+    AVPlayer * _player;
+    AVPlayerLayer * _playerLayer;
+}
 @end
 
 @implementation ViewController
@@ -19,5 +25,36 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    NSURL *videoURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"giphy" ofType:@"mp4"]];
+    
+    AVPlayerItem *item = [AVPlayerItem playerItemWithURL:videoURL];
+    _player = [AVPlayer playerWithPlayerItem:item];
+    
+    CALayer *superlayer = self.view.layer;
+    
+    AVPlayerLayer *playerLayer = [AVPlayerLayer playerLayerWithPlayer:_player];
+    [playerLayer setFrame:self.view.bounds];
+    playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    [superlayer addSublayer:playerLayer];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playerDidFinishPlaying:) name:AVPlayerItemDidPlayToEndTimeNotification object:item];
+    
+    [_player seekToTime:kCMTimeZero];
+    [_player play];
+}
+
+-(void)playerDidFinishPlaying:(NSNotification *)notification {
+    
+    [_player seekToTime:kCMTimeZero];
+    [_player play];
+}
+
+- (void)viewDidLayoutSubviews {
+    _playerLayer.frame = self.view.layer.bounds;
+    [super viewDidLayoutSubviews];
+}
 
 @end
